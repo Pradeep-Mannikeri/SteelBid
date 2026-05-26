c08ba9cb6f42dfc90f30c3f522b9e6f73f68c4d9
pradeepmannikeri <royalgroups247@gmail.com>
2026-05-26 11:55:48 +0530

    chore: only log stack trace for internal server errors

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,9 +1,14 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.error(err);
   const statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
   const msg = err.message || "Something went wrong, try again later";
+
+  if (statusCode === StatusCodes.INTERNAL_SERVER_ERROR) {
+    console.error(err);
+  } else {
+    console.warn(`${err.name || "Error"} (${statusCode}): ${msg}`);
+  }
 
   res.status(statusCode).json({ msg });
 };
